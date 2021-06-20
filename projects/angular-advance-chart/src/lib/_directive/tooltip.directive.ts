f0bb75bdbd69bfd3459b98801833d7f73ce6528d
somesh <someshkumarbhaskar@gmail.com>
2021-06-20 14:30:47 +0530

    updated tooltip

--- projects/angular-advance-chart/src/lib/_directive/tooltip.directive.ts
+++ projects/angular-advance-chart/src/lib/_directive/tooltip.directive.ts
@@ -5,23 +5,21 @@ import { Directive, Input, ElementRef, HostListener, Renderer2 } from '@angular/
 })
 export class TooltipDirective {
   @Input('tooltip') tooltipTitle: string;
-  @Input() placement: string = 'top';
   tooltip: HTMLElement;
-  offset = 10;
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    if (!this.tooltip) { this.show(); }
+  @HostListener('mouseenter', ['$event']) onMouseEnter(event:any) {
+    if (!this.tooltip) { this.show(event); }
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     if (this.tooltip) { this.hide(); }
   }
 
-  show() {
+  show(event) {
     this.create();
-    this.setPosition();
+    this.setPosition(event);
     this.renderer.addClass(this.tooltip, 'chart-tooltip-show');
   }
 
@@ -42,33 +40,14 @@ export class TooltipDirective {
 
     this.renderer.appendChild(document.body, this.tooltip);
     this.renderer.addClass(this.tooltip, 'chart-tooltip');
-    this.renderer.addClass(this.tooltip, `chart-tooltip-${this.placement}`);
   }
 
-  setPosition() {
-    const hostPos = this.el.nativeElement.getBoundingClientRect();
-
-    const tooltipPos = this.tooltip.getBoundingClientRect();
+  setPosition(event) {
 
     const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    let top, left;
-
-    if (this.placement === 'bottom') {
-      top = hostPos.bottom + this.offset;
-      left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
-    } else if (this.placement === 'left') {
-      top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
-      left = hostPos.left - tooltipPos.width - this.offset;
-    } else if (this.placement === 'right') {
-      top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
-      left = hostPos.right + this.offset;
-    } else {
-      // top
-      top = hostPos.top - tooltipPos.height - this.offset;
-      left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
-    }
-
+    let top=event.y;
+    let left=event.x
     this.renderer.setStyle(this.tooltip, 'top', `${top + scrollPos}px`);
     this.renderer.setStyle(this.tooltip, 'left', `${left}px`);
   }
